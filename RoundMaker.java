1164e04e99bc794cb896cca808127996a5500c5c
Phionx <shanjha.usa@gmail.com>
2016-05-14 17:17:37 -0400

    Parsing

--- RoundMaker.java
+++ RoundMaker.java
@@ -3,9 +3,50 @@ import java.io.*;
 import java.util.*;
 public class RoundMaker{
     public static void main(String [] args) {
+        List<List<String[]>> Data = new ArrayList<List<String[]>>();
+
+        List<String[]> Physics = new ArrayList<String[]>();
+        List<String[]> Mathematics = new ArrayList<String[]>();
+        List<String[]> Biology = new ArrayList<String[]>();
+        List<String[]> Chemistry = new ArrayList<String[]>();
+        List<String[]> EarthSpace = new ArrayList<String[]>();
+        List<String[]> Energy = new ArrayList<String[]>();
         try {
-        CSVReader reader = new CSVReader(new FileReader("Questions.csv"));
-        List myEntries = reader.readAll();
+            CSVReader reader = new CSVReader(new FileReader("yourfile.csv"));
+            String [] nextLine;
+            
+            while ((nextLine = reader.readNext()) != null) {
+                //choose which List to add the Question Pairs to
+                switch(nextLine[2]){
+                    case "Physics":
+                        Physics.add(nextLine);
+                        break;
+                    case "Chemistry":
+                        Chemistry.add(nextLine);
+                        break;
+                    case "Mathematics":
+                        Mathematics.add(nextLine);
+                        break;
+                    case "Earth and Space Science":
+                        EarthSpace.add(nextLine);
+                        break;
+                    case "Energy":
+                        Energy.add(nextLine);
+                        break;
+                    case "Biology":
+                        Biology.add(nextLine);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            //Add the individual Lists to the main Data List
+            Data.add(Physics);
+            Data.add(Mathematics);
+            Data.add(Biology);
+            Data.add(Chemistry);
+            Data.add(EarthSpace);
+            Data.add(Energy);
         } catch (IOException e){
         }
     }
