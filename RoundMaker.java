110516c28cc8b3b05978d6ef81b80453812b52f1
Phionx <shanjha.usa@gmail.com>
2016-05-14 21:31:01 -0400

    basic itext working

--- RoundMaker.java
+++ RoundMaker.java
@@ -1,7 +1,17 @@
 import com.opencsv.CSVReader;
 import java.io.*;
 import java.util.*;
+
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfWriter;
 public class RoundMaker{
+    
     public static void main(String [] args) {
         List<List<String[]>> Data = new ArrayList<List<String[]>>();
 
@@ -49,5 +59,47 @@ public class RoundMaker{
             Data.add(Energy);
         } catch (IOException e){
         }
+
+        Font body = FontFactory.getFont(FontFactory.COURIER, 10, Font.NORMAL, new CMYKColor(255, 255, 255, 0));
+
+        //Writing PDFs
+        Document document = new Document();
+        try
+        {
+
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Physics.pdf"));
+            document.open();
+            
+            //main paragraph to start of main chapter
+            Paragraph physicsTitle = new Paragraph("PHYSICS", body);
+            //write chapter
+            Chapter physicsText = new Chapter(physicsTitle, 1);
+            physicsText.setNumberDepth(0);
+
+            Paragraph question1Title = new Paragraph("Test", body);
+            Section question1 = physicsText.addSection(question1Title);
+            
+            Paragraph question1Content = new Paragraph("Test Body", body);
+            question1.add(question1Content);
+
+            document.add(physicsText);
+
+
+            //Set attributes here
+            document.addAuthor("Shantanu Jha");
+            document.addCreationDate();
+            document.addCreator("https://sites.google.com/site/stuyvesantsciencebowl/");
+            document.addTitle("Physics");
+            document.addSubject("Physics Questions");
+            
+            
+            document.close();
+            writer.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+
     }
 }
